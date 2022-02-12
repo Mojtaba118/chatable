160350246ddc2342f312ec699fe4256f731fce35
Mojtaba <seyyedmojtabahoseyni92@gmail.com>
2022-02-12 17:01:54 +0330

    add migration config to service provider

--- src/ChatableServiceProvider.php
+++ src/ChatableServiceProvider.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace Mojtaba\Chatable;
 
 use Illuminate\Support\ServiceProvider;
@@ -7,11 +8,26 @@ class ChatableServiceProvider extends ServiceProvider
 {
     public function boot()
     {
-
+        $this->definePublishableConfigs();
+        $this->defineMigrations();
     }
 
     public function register()
     {
 
+    }
+
+    public function defineMigrations()
+    {
+        $this->loadMigrationsFrom(__DIR__ . "/../database/migrations");
+    }
+
+    public function definePublishableConfigs()
+    {
+        if (app()->runningInConsole()) {
+            $this->publishes([
+                __DIR__ . "/../database/migrations" => database_path('migrations')
+            ], 'chatable-migrations');
+        }
     }
 }
