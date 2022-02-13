7b00189b0180ab5f7adc89d8303d6a2a0d7ee170
Mojtaba <seyyedmojtabahoseyni92@gmail.com>
2022-02-13 17:06:30 +0330

    improve chat exception

--- src/Exceptions/ChatNotFoundException.php
+++ src/Exceptions/ChatNotFoundException.php
@@ -2,7 +2,12 @@
 
 namespace Mojtaba\Chatable\Exceptions;
 
+use Throwable;
+
 class ChatNotFoundException extends \Exception
 {
-
+    public function __construct($message = "Chat Not Found", $code = 404, Throwable $previous = null)
+    {
+        parent::__construct($message, $code, $previous);
+    }
 }
