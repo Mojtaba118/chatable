1718ed7ea50377c8a1be28d82cf6810664a9fc5e
Mojtaba <seyyedmojtabahoseyni92@gmail.com>
2022-02-13 17:07:07 +0330

    add chat service

---
+++ src/Services/ChatService.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Mojtaba\Chatable\Services;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
+use Mojtaba\Chatable\Exceptions\ChatNotFoundException;
+use Mojtaba\Chatable\Models\Chat;
+
+class ChatService
+{
+    public static function chats(Model $user)
+    {
+        return Chat::query()->where(function ($q) use ($user) {
+            $q->where('sender_id', $user->id)
+                ->where('sender_type', get_class($user));
+
+        })->orWhere(function ($q) use ($user) {
+            $q->where('receiver_id', $user->id)
+                ->where('receiver_type', get_class($user));
+        });
+    }
+
+    public static function chatWith(Model $sender, Model $receiver)
+    {
+        return $sender->senderChats()->create([
+            'uuid' => Str::uuid()->toString(),
+            'receiver_id' => $receiver->id,
+            'receiver_type' => get_class($receiver),
+        ]);
+    }
+
+    public static function chatExists(Model $user, Chat $chat): bool
+    {
+        return !!static::chats($user)->where('id', $chat->id)->first();
+    }
+}
