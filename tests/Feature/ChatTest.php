162b49109cb18346614cf7d2b41942460b2d45d1
Mojtaba <seyyedmojtabahoseyni92@gmail.com>
2022-02-13 17:07:27 +0330

    add first test test

---
+++ tests/Feature/ChatTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Mojtaba\Chatable\Tests\Feature;
+
+use Mojtaba\Chatable\Tests\TestCase;
+
+class ChatTest extends TestCase
+{
+    public function test_user_can_start_chat()
+    {
+        $this->assertTrue(true);
+    }
+}
