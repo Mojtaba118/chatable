c2cc8814815449347398e02c6b8fe2f5a4a65532
Mojtaba <40440450+Mojtaba118@users.noreply.github.com>
2022-02-14 01:12:08 +0330

    add file upload

--- src/Helper/ChatMessageBuilder.php
+++ src/Helper/ChatMessageBuilder.php
@@ -2,19 +2,24 @@
 
 namespace Mojtaba\Chatable\Helper;
 
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Http\UploadedFile;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Str;
 use Mojtaba\Chatable\Models\Chat;
+use Mojtaba\Chatable\Models\Media;
 use Mojtaba\Chatable\Models\Message;
-use Symfony\Component\HttpFoundation\File\UploadedFile;
 
 class ChatMessageBuilder
 {
     private Chat $chat;
     private Model $sender;
-    private Message $replyMessage;
-    private UploadedFile $file;
-    private string $caption;
-    private string $message;
+    private ?Message $replyMessage = null;
+    private ?UploadedFile $file = null;
+    private string $messageContent;
+
+    private Message $message;
 
     public function setChat(Chat $chat): self
     {
@@ -32,21 +37,17 @@ class ChatMessageBuilder
 
     /**
      * @param UploadedFile $file
-     * @param string|null $caption
-     *
      * @return static
      */
-    public function setFile(UploadedFile $file, string $caption = null): self
+    public function setFile(UploadedFile $file): self
     {
         $this->file = $file;
-        $this->caption = $caption;
-
         return $this;
     }
 
-    public function setMessage(string $message): self
+    public function setMessageContent(string $messageContent): self
     {
-        $this->message = $message;
+        $this->messageContent = $messageContent;
 
         return $this;
     }
@@ -69,15 +70,43 @@ class ChatMessageBuilder
             $messageData['sender_type'] = get_class($this->sender);
         }
 
-        if ($this->message) {
-            $messageData['message'] = $this->message;
+        if ($this->messageContent) {
+            $messageData['message'] = $this->messageContent;
         }
 
         if ($this->replyMessage) {
             $messageData['reply_id'] = $this->replyMessage->id;
         }
 
-        return $this->chat->messages()->create($messageData);
+        return DB::transaction(function () use ($messageData) {
+            $this->message = $this->chat->messages()->create($messageData);
+
+            if ($this->file) {
+                $this->storeFiles($this->message);
+                $this->message->load('medias');
+            }
+
+            if ($this->replyMessage)
+                $this->message->load('reply');
+
+            return $this->message;
+        });
+    }
+
+    private function storeFiles(Message $message): Media
+    {
+        $now = now();
+
+        $filePath = "chat_medias/$now->year/$now->month/" . $this->chat->uuid;
+        $fileName = $now->year . '_' . $now->month . '_' . $now->day . '_' . $this->file->hashName();
+
+        Storage::drive('public')->putFileAs($filePath, $this->file, $fileName);
+
+        return $message->medias()->create([
+            'uuid' => Str::uuid()->toString(),
+            'path' => "$filePath/$fileName",
+            "type" => $this->file->getMimeType()
+        ]);
     }
 
 
