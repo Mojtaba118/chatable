33546fff4519ed806374903743a36b82bc6e1b4d
Mojtaba <seyyedmojtabahoseyni92@gmail.com>
2022-02-13 17:06:17 +0330

    add send message event

---
+++ src/Events/SendChatMessageEvent.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Mojtaba\Chatable\Events;
+
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Broadcasting\PresenceChannel;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+use Mojtaba\Chatable\Models\Chat;
+use Mojtaba\Chatable\Models\Message;
+
+class SendChatMessageEvent implements ShouldBroadcast
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    private Message $message;
+
+    /**
+     * Create a new event instance.
+     *
+     * @return void
+     */
+    public function __construct(Message $message)
+    {
+        $this->message = $message;
+    }
+
+    /**
+     * Get the channels the event should broadcast on.
+     *
+     * @return \Illuminate\Broadcasting\Channel|array
+     */
+    public function broadcastOn()
+    {
+        return new Channel('chat-message');
+    }
+
+    /**
+     * Get the data to broadcast.
+     *
+     * @return array
+     */
+    public function broadcastWith()
+    {
+        return ['message' => $this->message->message];
+    }
+}
