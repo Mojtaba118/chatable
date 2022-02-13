418d7b3d6ff6e9ec728789239fc98bbcba5731ad
Mojtaba <seyyedmojtabahoseyni92@gmail.com>
2022-02-13 17:06:59 +0330

    add caht message service

---
+++ src/Services/ChatMessageService.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Mojtaba\Chatable\Services;
+
+use Illuminate\Database\Eloquent\Model;
+use Mojtaba\Chatable\Helper\ChatMessageBuilder;
+use Mojtaba\Chatable\Models\Chat;
+use Mojtaba\Chatable\Models\Message;
+
+class ChatMessageService
+{
+    public static function storeMessage(Chat $chat, Model $sender, $data): Message
+    {
+        $messageBuilder = (new ChatMessageBuilder)->setChat($chat)
+            ->setSender($sender);
+
+        if (isset($data['file'])) {
+            $messageBuilder->setFile($data['file'], $data['message'] ?? null);
+        } else {
+            $messageBuilder->setMessage($data['message']);
+        }
+
+        if (isset($data['reply_id'])) {
+            $message = Message::where([
+                'chatable_id' => $chat->id,
+                'chatable_type' => get_class($chat),
+            ])->findOrFail($data['reply_id']);
+
+            $messageBuilder->setReplyTo($message);
+        }
+
+        return $messageBuilder->store();
+    }
+}
