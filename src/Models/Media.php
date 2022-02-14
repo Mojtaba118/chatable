e099c70b2016fd06dc67d263954c045aa082cc48
Mojtaba <seyyedmojtabahoseyni92@gmail.com>
2022-02-14 17:31:35 +0330

    add media attributes

--- src/Models/Media.php
+++ src/Models/Media.php
@@ -3,12 +3,29 @@
 namespace Mojtaba\Chatable\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Storage;
+use Mojtaba\Chatable\Helper\MimeTypeHelper;
 
 class Media extends Model
 {
     protected $guarded = ['id'];
 
     protected $table = 'chatable_medias';
+
+    protected $appends = [
+        'file_url',
+        'file_type'
+    ];
+
+    public function getFileUrlAttribute()
+    {
+        return Storage::drive(config('chatable.storage_driver'))->url($this->path);
+    }
+
+    public function getFileTypeAttribute()
+    {
+        return MimeTypeHelper::getType($this->type);
+    }
 
     public function message()
     {
